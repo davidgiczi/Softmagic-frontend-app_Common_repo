b3d832278092449a1937fd26edee8da91fd3b2b2
david.giczi@gmail.com <david.giczi@gmail.com>
2022-04-18 00:31:21 +0200

    Docs-downloading

--- src/pages/Docs.js
+++ src/pages/Docs.js
@@ -13,14 +13,28 @@ import { useState, useEffect } from "react";
       .then(data =>  setDocs(data));
     }, [])
 
-    const download = () => {
+    const download = (id, docName) => {
+
        if(window.confirm("Bizos, hogy letölti a dokumentumot?")){
-           alert("A dokumentumok letöltése funkció fejlesztés alatt.")
-       };
-    }
+        
+       fetch("https://backend.softmagic.hu/softmagic/doc?docId=" + id, {
+        method: "GET",
+        cache: "no-cache",
+      })
+      .then(response => response.blob())
+      .then(doc => { 
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(doc);
+        link.download = docName;
+        document.body.append(link);
+        link.click();
+        link.remove();
+      })
+      }}
     return (<div className="Doc-group">
-    {docs.map((doc) =><p key={doc.id} onClick={download} className="Doc-row">{doc.title + '.'}<b>{doc.extension}</b></p>)}
+    {docs.map((doc) =><p key={doc.id} 
+    onClick={() => download(doc.id, doc.title + '.' + doc.extension)} className="Doc-row">{doc.title + '.'}<b>{doc.extension}</b></p>)}
     </div>);
- }
+ } 
 
 export default Docs;
